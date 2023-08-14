f1d3c0194c2796cafea54730fbbc6d11d83a0923
jakejones2 <jakejones1998@hotmail.co.uk>
2023-08-14 13:56:13 +0100

    remove 404 test

--- __tests__/topics.test.js
+++ __tests__/topics.test.js
@@ -41,8 +41,5 @@ describe("topics", () => {
           });
         });
     });
-    test("GET 404 from bad path", () => {
-      return request(app).get("/api/pancakes").expect(404);
-    });
   });
 });
