61ef33af14057bc4c96b8332ce76b788f939425f
jakejones2 <jakejones1998@hotmail.co.uk>
2023-08-14 14:23:04 +0100

    add /api endpoint

--- __tests__/topics.test.js
+++ __tests__/topics.test.js
@@ -12,34 +12,32 @@ beforeEach(() => {
   return seed(data);
 });
 
-describe("topics", () => {
-  describe("GET /api/topics", () => {
-    test("GET 200 from /api/topics", () => {
-      return request(app).get("/api/topics").expect(200);
-    });
-    test("GET data from /api/topics", () => {
-      return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({ body: { topics } }) => {
-          expect(topics.length > 0).toBe(true);
-          topics.forEach((topic) => {
-            expect(topic).toHaveProperty("slug", expect.any(String));
-            expect(topic).toHaveProperty("description", expect.any(String));
-          });
-          expect(topics[0]).toMatchObject({
-            description: "The man, the Mitch, the legend",
-            slug: "mitch",
-          });
-          expect(topics[1]).toMatchObject({
-            description: "Not dogs",
-            slug: "cats",
-          });
-          expect(topics[2]).toMatchObject({
-            description: "what books are made of",
-            slug: "paper",
-          });
+describe("GET /api/topics", () => {
+  test("GET 200 from /api/topics", () => {
+    return request(app).get("/api/topics").expect(200);
+  });
+  test("GET data from /api/topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length > 0).toBe(true);
+        topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug", expect.any(String));
+          expect(topic).toHaveProperty("description", expect.any(String));
         });
-    });
+        expect(topics[0]).toMatchObject({
+          description: "The man, the Mitch, the legend",
+          slug: "mitch",
+        });
+        expect(topics[1]).toMatchObject({
+          description: "Not dogs",
+          slug: "cats",
+        });
+        expect(topics[2]).toMatchObject({
+          description: "what books are made of",
+          slug: "paper",
+        });
+      });
   });
 });
