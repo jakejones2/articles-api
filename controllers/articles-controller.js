9d98801be444972b27e0a1ea58eee8bafb2899d9
jakejones2 <jakejones1998@hotmail.co.uk>
2023-08-15 15:05:54 +0100

    add queries

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -4,6 +4,8 @@ const {
   updateArticleById,
 } = require("../models/articles-model");
 
+const { selectTopics } = require("../models/topics-model");
+
 function getArticleById(req, res, next) {
   selectArticleById(req.params.article_id)
     .then((article) => {
@@ -13,7 +15,39 @@ function getArticleById(req, res, next) {
 }
 
 function getArticles(req, res, next) {
-  selectArticles()
+  selectTopics()
+    .then((topics) => {
+      return Promise.resolve(
+        topics.map((topic) => {
+          return topic.slug;
+        })
+      );
+    })
+    .then((topicList) => {
+      const topic = req.query.topic;
+      const sort_by = req.query.sort_by ? req.query.sort_by : "created_at";
+      const order = req.query.order ? req.query.order.toUpperCase() : "DESC";
+      const validSortBy = [
+        "article_id",
+        "title",
+        "topic",
+        "author",
+        "body",
+        "created_at",
+        "votes",
+        "comment_count",
+        "article_img_url",
+      ];
+      if (
+        (!topicList.includes(topic) && topic) ||
+        !validSortBy.includes(sort_by) ||
+        !["ASC", "DESC"].includes(order)
+      ) {
+        return Promise.reject({ status: 404, msg: "Query not available" });
+      } else {
+        return selectArticles(topic, sort_by, order);
+      }
+    })
     .then((articles) => {
       res.status(200).send({ articles });
     })
