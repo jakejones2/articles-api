9ee971790e631357f84d5f9b83a0e2210dd4ded6
jakejones2 <jakejones1998@hotmail.co.uk>
2023-08-19 12:02:18 +0100

    delete /account endpoint

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -3,8 +3,5 @@ const { authUser } = require("../controllers/auth-controller");
 const { verifyJWT } = require("../middleware/verifyJWT");
 
 authRouter.post("/", authUser);
-authRouter.get("/account", verifyJWT, (req, res, next) => {
-  res.status(200).send({ "logged-in": true });
-});
 
 module.exports = authRouter;
